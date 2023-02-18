f81dd038e7c6d0b40215bf6ccfc89d5237f4925a
James Kelly <jk377y@gmail.com>
2023-02-18 01:33:14 -0600

    fixed auto seeder

--- models/champion.js
+++ models/champion.js
@@ -4,127 +4,43 @@ const sequelize = require('../config/connection');
 class Champion extends Model {}
 
 Champion.init(
-    {
-        id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true,
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        lore: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        skins:{
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        spells:{ 
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            [0]:{
-                id: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                name:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                description:{
-                    type: DataTypes.TEXT,
-                    allowNull: false,
-                },
-                image:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-            [1]:{
-                id: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                name:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                description:{
-                    type: DataTypes.TEXT,
-                    allowNull: false,
-                },
-                image:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-            [2]:{
-                id: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                name:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                description:{
-                    type: DataTypes.TEXT,
-                    allowNull: false,
-                },
-                image:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-            [3]:{
-                id: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                name:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-                description:{
-                    type: DataTypes.TEXT,
-                    allowNull: false,
-                },
-                image:{
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                },
-            },
-        },
-        passive:{
-            type: DataTypes.STRING,
-            allowNull: false,
-            name:{
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            description:{
-                type: DataTypes.TEXT,
-                allowNull: false,
-            },
-            image:{
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-        },
+  {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
     },
-    {
-		sequelize,
-		freezeTableName: true,
-		underscored: false,
-		modelName: 'champion',
-	} 
-)
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lore: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    skins: {
+      type: DataTypes.JSON, // change to JSON data type
+      allowNull: false,
+    },
+    spells: {
+      type: DataTypes.JSON, // change to JSON data type
+      allowNull: false,
+    },
+    passive: {
+      type: DataTypes.JSON, // change to JSON data type
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true, // change to underscored naming convention
+    modelName: 'champion',
+  } 
+);
 
 module.exports = Champion;
